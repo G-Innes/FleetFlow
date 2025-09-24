cef76ca4c596e80e16d36c4cd670e6dd46478e0d
Grant Innes <129431946+nonProphet-90@users.noreply.github.com>
2025-09-24 13:28:37 +0300

    fix(vercel): force https URLs in production and ensure vite assets resolve over https

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -18,7 +18,7 @@
 
         <!-- Scripts -->
         @routes
-        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"], asset: true)
         @inertiaHead
     </head>
     <body class="font-sans antialiased bg-fleet-dark text-fleet-text min-h-screen">
